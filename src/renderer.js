5f2914689d3afd67ef78a20672ec9a59157e6de5
Alexander Gabriel <alex.barbalex@gmail.com>
2019-10-08 12:51:47 +0200

    try to import native module

--- src/renderer.js
+++ src/renderer.js
@@ -27,5 +27,6 @@
  */
 import React from 'react'
 import { render } from 'react-dom'
+import betterSqlite from 'better-sqlite3'
 
 render(<div>hello world</div>, document.getElementById('root'))
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -4,6 +4,11 @@ rules.push({
   test: /\.css$/,
   use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
 })
+rules.push({
+  test: /\.jsx?$/,
+  exclude: /node_modules/,
+  use: [{ loader: 'babel-loader' }],
+})
 
 module.exports = {
   // Put your normal webpack config below here
